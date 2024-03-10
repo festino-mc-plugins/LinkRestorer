6478512a9244b1baa65e3371b9b27157a1b0acbf
Festino <offestashka@mail.ru>
2024-03-10 16:03:35 +0300

    feat: add TextStyleSwitch

---
+++ src/main/java/com/festp/parsing/TextStyleSwitch.java
@@ -0,0 +1,12 @@
+package com.festp.parsing;
+
+public class TextStyleSwitch {
+	public final TextStyle style;
+	public final int index;
+	
+	public TextStyleSwitch(TextStyle style, int index)
+	{
+		this.style = style;
+		this.index = index;
+	}
+}
