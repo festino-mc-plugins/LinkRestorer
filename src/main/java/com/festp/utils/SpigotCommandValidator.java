6e4e1071b97feb92d0fa271bc1fda2a9b9797c42
Festino <offestashka@mail.ru>
2024-05-17 19:21:24 +0300

    fix: log found commands only in debug mode

--- src/main/java/com/festp/utils/SpigotCommandValidator.java
+++ src/main/java/com/festp/utils/SpigotCommandValidator.java
@@ -7,15 +7,27 @@ import org.bukkit.Bukkit;
 import org.bukkit.help.HelpTopic;
 
 import com.festp.Logger;
+import com.festp.config.Config;
+import com.festp.config.Config.Key;
 
 public class SpigotCommandValidator implements CommandValidator
 {
+	private final Config config;
+	
 	// lazy initialization
 	private Collection<String> commands = new HashSet<>();
 	
+	public SpigotCommandValidator(Config config) {
+		this.config = config;
+	}
+	
 	@Override
 	public boolean commandExists(String command) {
-		if (commands.isEmpty()) commands = getCommands();
+		if (commands.isEmpty()) {
+			commands = getCommands();
+			if (config.get(Key.LOG_DEBUG, true)) // TODO Logger.canLog(level)
+				Logger.info("Found commands: " + String.join(", ", commands));
+		}
 		
 		if (command.charAt(0) != '/') return false;
         return commands.contains(command.substring(1));
@@ -40,7 +52,6 @@ public class SpigotCommandValidator implements CommandValidator
                 }
             }
         }
-		Logger.info("Found commands: " + String.join(", ", commandsAndAliases));
 		return commandsAndAliases;
 	}
 }
